ea336a9bd9bc4672573588782deb4c63bc4abc2b
Sergey Kovylov <sere99ka@gmail.com>
2021-10-01 10:43:18 +0300

    Добавил все необходимые переменные и данные. Вывел всё в консоль

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const title = "Супер заголовок";
-let screens = 3;
+const title = "Проект JavaScript 0.1";
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 1200;
-let rollback = 1;
-const fullPrice = screenPrice * 1.2;
-let adaptive = "Не хочу писать адаптив =)";
-console.log( adaptive );
-alert( title );
+let rollback = 13;
+const fullPrice = 15000;
+let adaptive = true;
+
+console.log( typeof title);
+console.log( typeof fullPrice);
+console.log( typeof adaptive);
+console.log( screens.length );
+console.log("Стоимость вёрстки экранов " +screenPrice+ " долларов");
+console.log("Стоимость разработки сайта " +fullPrice+ " долларов");
+
+let str = screens.toLowerCase()
+console.log(str.split());
+
+let rollbackPrice = fullPrice * (rollback/100)
+console.log(rollbackPrice);
