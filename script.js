a185ce99d84cd418e836d9d05c78745d3a9a59fb
Sergey Kovylov <sere99ka@gmail.com>
2021-10-11 19:04:36 +0300

    конфликт в ветке dev и lesson07

--- script.js
+++ script.js
@@ -1,74 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-let title;
-let screens;
-let screenPrice;
-let adaptive;
-let rollback = 13;
-let servicePercentPrice;
-let allServicePrices;
-let fullPrice;
-let service1;
-let service2;
-let servicePrice;
-
-//объявления функций
-
-const isNumber = function (num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-}
-
-const asking = function () {
-    title = prompt('Как называется Ваш проект?', 'Калькулятор вёрстки');
-    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-
-    do {
-        screenPrice = prompt('Сколько будет стоить данная работа?');
-    } while (!isNumber(screenPrice)) {
-
-    }
-
-    adaptive = confirm('Нужен ли адаптив на сайте?');
-}
-
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
-}
-
-const getRollbackMessage = function (price) {
-    if (price >= 30000) {
-        return 'Даём скидку в 10%'
-    } else if (price < 30000 && price >= 15000) {
-        return 'Даём скидку в 5%'
-    } else if (price < 15000 && price >= 0) {
-        return 'Скидка не предусмотрена'
-    } else
-        return 'Что то пошло не так';
-}
-
-const getAllServicePrices = function () {
-    let sum = 0;
-    for (let i = 0; i < 2; i++) {
-        if (i === 0) {
-            service1 = prompt('Какой тип дополнительной услуги нужен?', 'Метрика');
-            do {
-                servicePrice = prompt('Сколько это будет стоить?', '2030');
-            } while (!isNumber(servicePrice)) {
-
-            }
-        } else if (i === 1) {
-            service2 = prompt('Какой тип дополнительной услуги нужен?', 'Формы');
-            do {
-                servicePrice = prompt('Сколько это будет стоить?', '2030');
-            } while (!isNumber(servicePrice)) {
-
-            }
-        }
-        sum += +servicePrice
-    }
-    return sum;
-=======
 const appData = {
     title: '',
     screens: '',
@@ -171,7 +102,6 @@ const appData = {
             console.log('Ключ: ' + key + ' ' + 'Значение: ' + appData[key]);
         }
     }
->>>>>>> lesson07
 }
 
 function getFullPrice() {
